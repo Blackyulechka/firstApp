5b65f54d401cb5b50859aced57e3a7963783c3e0
юля <123@mail.ru>
2025-09-17 23:36:24 +0300

    second commit

--- app/src/main/java/com/example/task1/MainActivity.java
+++ app/src/main/java/com/example/task1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.task1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button buttonWorkOne = findViewById(R.id.button4);
+        Button buttonWorkTwo = findViewById(R.id.button5);
+        Button buttonWorkTree = findViewById(R.id.button6);
+        buttonWorkOne.setOnClickListener(new View.OnClickListener() {
+                                             @Override
+                                             public void onClick(View v) {
+                                                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                                                 startActivity(intent);
+                                             }
+                                         });
+        buttonWorkTwo.setOnClickListener(new View.OnClickListener() {
+                                             @Override
+                                             public void onClick(View v) {
+                                                 Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                                                 startActivity(intent);
+                                             }
+                                         });
+        buttonWorkTree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, work_three.class);
+                startActivity(intent);
+            }
         });
     }
 }
